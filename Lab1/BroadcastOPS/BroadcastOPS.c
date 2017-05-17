2dd49afaad84eab898d55d792a0c1cd9931e3d8c
Andrew Yefanov <1134togo@gmail.com>
2017-05-17 12:22:10 +0300

    Broadcast tested and refactored

--- Lab1/BroadcastOPS/BroadcastOPS.c
+++ Lab1/BroadcastOPS/BroadcastOPS.c
@@ -30,40 +30,48 @@ int main(int argc, char **argv) {
     clock_t start, end;
 
     double timeForNSendings, timeForNBroadcasts;
-    if (world_size > 1) {
-        if (rank == 0) {
-            printf("World size is %d\n", world_size);
-            printf("Number of tries is %d\n", numberOfSendings);
-
-            start = clock();
-
-            for (int sending = 0; sending < numberOfSendings; ++sending) {
-                for (int destination = 1; destination < world_size; ++destination) {
-                    MPI_Send(NULL, 0, MPI_BYTE, destination, 1, MPI_COMM_WORLD);
-                }
-            }
-            end = clock();
-            timeForNSendings = ((double) (end - start) / 1000000.0F) * 1000;
-            printf("time for %d send operations = %f \n", numberOfSendings, timeForNSendings);
-            printf("OPS = %f\n", timeForNSendings / (double) numberOfSendings);
-
-        } else {
-            for (int sending = 0; sending < numberOfSendings; ++sending) {
-                MPI_Recv(NULL, 0, MPI_BYTE, 0, 1, MPI_COMM_WORLD, &status);
-            }
-        }
-    } else {
+    if (world_size < 2) {
         printf("World is too small\n");
         return 1;
     }
 
-    start = clock();
+    int isMaster = rank == 0;
+    if (isMaster) {
+        printf("World size is %d\n", world_size);
+        printf("Number of tries is %d\n", numberOfSendings);
+
+        start = clock();
+
+        for (int sending = 0; sending < numberOfSendings; ++sending) {
+            for (int destination = 1; destination < world_size; ++destination) {
+                MPI_Send(NULL, 0, MPI_BYTE, destination, 1, MPI_COMM_WORLD);
+            }
+        }
+        end = clock();
+        timeForNSendings = ((double) (end - start) / 1000000.0F) * 1000;
+        printf("time for %d send operations = %f \n", numberOfSendings, timeForNSendings);
+        printf("OPS = %f\n", (double) numberOfSendings / timeForNSendings);
+
+    } else {
+        for (int sending = 0; sending < numberOfSendings; ++sending) {
+            MPI_Recv(NULL, 0, MPI_BYTE, 0, 1, MPI_COMM_WORLD, &status);
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (isMaster) {
+        start = clock();
+    }
     for (int sending = 0; sending < numberOfSendings; ++sending) {
         MPI_Bcast(NULL, 0, MPI_BYTE, 0, MPI_COMM_WORLD);
     }
-    end = clock();
-    timeForNBroadcasts = ((double) (end - start) / 1000000.0F) * 1000;
-    printf("time for %d broadcast operations = %f \n", numberOfSendings, timeForNBroadcasts);
+    if (isMaster) {
+        end = clock();
+        timeForNBroadcasts = ((double) (end - start) / 1000000.0F) * 1000;
+        printf("time for %d broadcast operations = %f \n", numberOfSendings, timeForNBroadcasts);
+        printf("OPS = %f\n", (double) numberOfSendings / timeForNBroadcasts);
+    }
 
     MPI_Finalize();
 }
