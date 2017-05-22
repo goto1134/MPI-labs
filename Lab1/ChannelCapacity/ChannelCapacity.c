6694cd97d98f9a5dc665ce43f8af6b04eb0bde83
Andrew Yefanov <1134togo@gmail.com>
2017-05-23 01:04:23 +0300

    channel capacity time output changed to millis C 90 standard applied

--- Lab1/ChannelCapacity/ChannelCapacity.c
+++ Lab1/ChannelCapacity/ChannelCapacity.c
@@ -1,73 +1,71 @@
 //
 // Created by Andrew on 23.04.2017.
 //
-
-//
-// Created by Andrew on 22.04.2017.
-//
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(int argc, char **argv) {
-	if(argc < 2) {
-		printf("usage: MPI_ChannelCapacity <number-of-sendings> <number-of-bytes>");
-		return 1;
-	}
-	int numberOfSendings, numberOfBytes;
-	if(sscanf(argv[1], "%i", &numberOfSendings) != 1) {
-		fprintf(stderr, "error - not an integer");
-		return 1;
-	}
-	if(sscanf(argv[2], "%i", &numberOfBytes) != 1) {
-		fprintf(stderr, "error - not an integer");
-		return 1;
-	}
+    if (argc < 2) {
+        printf("usage: MPI_ChannelCapacity <number-of-sendings> <number-of-bytes>");
+        return 1;
+    }
+    int numberOfSendings, numberOfBytes;
+    if (sscanf(argv[1], "%i", &numberOfSendings) != 1) {
+        fprintf(stderr, "error - not an integer");
+        return 1;
+    }
+    if (sscanf(argv[2], "%i", &numberOfBytes) != 1) {
+        fprintf(stderr, "error - not an integer");
+        return 1;
+    }
 
-	char *bytes = malloc(numberOfBytes * sizeof(char));
-	MPI_Init(&argc, &argv);
+    char *bytes = malloc(numberOfBytes * sizeof(char));
+    MPI_Init(&argc, &argv);
 
-	// Get the number of processes
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	// Get the rank of the process
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // Get the rank of the process
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	MPI_Status status;
-	if(world_size > 1) {
-		if(rank == 0) {
-			printf("World size is %d\n", world_size);
-			printf("Number of tries is %d\n", numberOfSendings);
-			for(int destination = 1; destination < world_size; ++destination) {
-				printf("Counting channel capacity with processor %d of %d\n", destination, world_size - 1);
-				double timeForNSendings = 0;
+    MPI_Status status;
+    if (world_size > 1) {
+        if (rank == 0) {
+            printf("World size is %d\n", world_size);
+            printf("Number of tries is %d\n", numberOfSendings);
+            int destination;
+            for (destination = 1; destination < world_size; ++destination) {
+                printf("Counting channel capacity with processor %d of %d\n", destination, world_size - 1);
+                double timeForNSendings = 0;
 
-				clock_t start, end;
-				start = clock();
-				for(int sending = 0; sending < numberOfSendings; ++sending) {
-					MPI_Send(bytes, numberOfBytes, MPI_BYTE, destination, 1, MPI_COMM_WORLD);
-					MPI_Recv(bytes, numberOfBytes, MPI_BYTE, destination, 1, MPI_COMM_WORLD, &status);
-				}
-				end = clock();
-				timeForNSendings = ((double) (end - start) / 1000000.0F) * 1000;
-				printf("time for %d cycles with %d = %f \n", numberOfSendings, destination, timeForNSendings);
-				printf("Channel capacity = %f b/s\nq",
-					   ((double) (2 * numberOfSendings) / (double) (end - start) * numberOfBytes));
-			}
-		} else {
-			for(int sending = 0; sending < numberOfSendings; ++sending) {
-				MPI_Recv(bytes, numberOfBytes, MPI_BYTE, 0, 1, MPI_COMM_WORLD, &status);
-				MPI_Send(bytes, numberOfBytes, MPI_BYTE, 0, 1, MPI_COMM_WORLD);
-			}
-		}
-	} else {
-		printf("World is too small\n");
-		return 1;
-	}
+                clock_t start, end;
+                start = clock();
+                int sending;
+                for (sending = 0; sending < numberOfSendings; ++sending) {
+                    MPI_Send(bytes, numberOfBytes, MPI_BYTE, destination, 1, MPI_COMM_WORLD);
+                    MPI_Recv(bytes, numberOfBytes, MPI_BYTE, destination, 1, MPI_COMM_WORLD, &status);
+                }
+                end = clock();
+                timeForNSendings = (double) (end - start);
+                printf("time for %d cycles with %d = %f millis \n", numberOfSendings, destination, timeForNSendings);
+                printf("Channel capacity = %f b/ms\n",
+                       ((double) numberOfBytes / timeForNSendings * 2 * numberOfSendings ));
+            }
+        } else {
+            int sending;
+            for (sending = 0; sending < numberOfSendings; ++sending) {
+                MPI_Recv(bytes, numberOfBytes, MPI_BYTE, 0, 1, MPI_COMM_WORLD, &status);
+                MPI_Send(bytes, numberOfBytes, MPI_BYTE, 0, 1, MPI_COMM_WORLD);
+            }
+        }
+    } else {
+        printf("World is too small\n");
+        return 1;
+    }
 
-//	MPI_Bcast(NULL, 0, MPI_BYTE, 0, MPI_COMM_WORLD);
-	MPI_Finalize();
+    MPI_Finalize();
 }
