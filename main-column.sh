061875cd8a98feefe50d79fa07be90b4a3587163
Andrew Yefanov <1134togo@gmail.com>
2017-06-22 20:36:10 +0300

    Скрипт для запуска параметризирован.

--- main-column.sh
+++ main-column.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mpicc /home/andrey.efanov/Lab2/MainElementInColumn/MainElementInColumn.c -o /home/andrey.efanov/Lab2/MainElementInColumn/main-column.out -lm
 
-qsub -l nodes=3:ppn=1 /home/andrey.efanov/Lab2/MainElementInColumn/main-column.sh
+qsub -l nodes=$1:ppn=$2 /home/andrey.efanov/Lab2/MainElementInColumn/main-column.sh
